84de5b4a76eac981a2462d204d5038293f64de90
Robin Lecullier <lecullier.robin@gmail.com>
2023-09-07 13:34:10 +0200

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div className="footer-container">
+            <p>Projet réalisé par DonkeyGeek</p>
+            <p>Les icones sont prises sur iconFinder.com</p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
