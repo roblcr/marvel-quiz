91f8857db98ad404d40ae01ad89f20ba44ec34a7
Robin Lecullier <lecullier.robin@gmail.com>
2023-09-07 17:14:50 +0200

    add img wolverine with useEffect

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useRef, useEffect, useState, Fragment } from 'react'
 
 const Landing = () => {
-  return (
-    <main className="welcomePage">
 
+    const [btn, setBtn] = useState(false);
+
+    const refWolverine = useRef(null)
+
+    useEffect(() => {
+        refWolverine.current.classList.add("startingImg");
+        setTimeout(() => {
+            refWolverine.current.classList.remove("startingImg");
+            setBtn(true);
+        }, 3000);
+
+    }, [])
+
+    const displayBtn = btn && (
+        <Fragment>
+            <div className='leftBox'>
+                <button className='btn-welcome'>Inscription</button>
+            </div>
+            <div className='rightBox'>
+                <button className='btn-welcome'>Connexion</button>
+            </div>
+        </Fragment>
+    )
+
+
+  return (
+    <main ref={refWolverine} className="welcomePage">
+        { displayBtn }
     </main>
   )
 }
