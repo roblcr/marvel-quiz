3cfeab42fce9dcccc359e62a57957c741db7cdcd
Robin Lecullier <lecullier.robin@gmail.com>
2023-09-11 18:04:58 +0200

    SignUp with firebase auth

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,18 +1,20 @@
-import app from 'firebase/app';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const config = {
-    apiKey: "AIzaSyChsI0H07-JdHNtYJ9WL-8uEbFnuIlVRPw",
-    authDomain: "marvel-quiz-b4199.firebaseapp.com",
-    projectId: "marvel-quiz-b4199",
-    storageBucket: "marvel-quiz-b4199.appspot.com",
-    messagingSenderId: "396426678326",
-    appId: "1:396426678326:web:9cacc5e16a191aa9a54897"
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChsI0H07-JdHNtYJ9WL-8uEbFnuIlVRPw",
+  authDomain: "marvel-quiz-b4199.firebaseapp.com",
+  projectId: "marvel-quiz-b4199",
+  storageBucket: "marvel-quiz-b4199.appspot.com",
+  messagingSenderId: "396426678326",
+  appId: "1:396426678326:web:9cacc5e16a191aa9a54897"
 };
 
-class Firebase {
-    constructor() {
-        app.initializeApp()
-    }
-}
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-export default Firebase
+export const auth = getAuth(app)
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,10 +1,84 @@
+import { createUserWithEmailAndPassword } from 'firebase/auth'
 import React from 'react'
+import { useState } from 'react'
+import { auth } from '../Firebase/firebase'
 
 const SignUp = () => {
+
+    const data = {
+        pseudo: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    }
+
+    const [loginData, setLoginData] = useState(data)
+
+    const handleChange = e => {
+        setLoginData({...loginData, [e.target.id]: e.target.value})
+    } 
+
+    const handleSignUp = (e) => {
+        e.preventDefault();
+      
+        if (password !== confirmPassword) {
+          alert("Les mots de passe ne correspondent pas.");
+          return;
+        }
+      
+        createUserWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+            console.log(userCredential);
+            // Gérer la réussite de l'inscription, par exemple, rediriger l'utilisateur vers la page de connexion.
+          })
+          .catch((error) => {
+            console.log(error);
+            if (error.code === "auth/email-already-in-use") {
+              alert("Cet e-mail est déjà utilisé. Veuillez utiliser un autre e-mail.");
+            } else {
+              alert("Erreur lors de l'inscription. Veuillez réessayer.");
+            }
+          });
+      };
+
+    const { pseudo, email, password, confirmPassword } = loginData
+
+    const btn = pseudo === '' || email === '' || password === '' || password !== confirmPassword 
+    ? <button disabled>Inscription</button> : <button>Inscription</button>
+
   return (
     <div className='signUpLoginBox'>
         <div className="slContainer">
-            SignUp
+            <div className="formBoxLeftSignup">
+
+            </div>
+            <div className="formBoxRight">
+                <div className="formContent">
+                    <form onSubmit={handleSignUp}>
+                        <h2>Inscription</h2>
+                        <div className="inputBox">
+                            <input onChange={handleChange} value={pseudo} type='text' id='pseudo' autoComplete='off' required/>
+                            <label htmlFor='pseudo'>Pseudo</label>
+                        </div>
+
+                        <div className="inputBox">
+                            <input onChange={handleChange} value={email} type='email' id='email' autoComplete='off' required/>
+                            <label htmlFor='email'>Email</label>
+                        </div>
+
+                        <div className="inputBox">
+                            <input onChange={handleChange} value={password} type='password' id='password' autoComplete='off' required/>
+                            <label htmlFor='password'>Mot de passe</label>
+                        </div>
+
+                        <div className="inputBox">
+                            <input onChange={handleChange} value={confirmPassword} type='password' id='confirmPassword' autoComplete='off' required/>
+                            <label htmlFor='confirmPassword'>Confirmer votre mot de passe</label>
+                        </div>
+                        {btn}
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
   )
