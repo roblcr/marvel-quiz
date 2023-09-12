6b6f6d89fa98272f6f33c02ae25047b69c544cdc
Robin Lecullier <lecullier.robin@gmail.com>
2023-09-12 17:20:11 +0200

    answer questions

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -12,12 +12,19 @@ class Quiz extends Component {
         storedQuestions: [],
         question: null,
         options: [],
-        idQuestion: 0
+        idQuestion: 0,
+        btnDisabled: true,
+        userAnswer: null,
+        score: 0
     }
+
+    storedDataRef = React.createRef()
 
     loadQuestions = quizz => {
         const fetchedArrayQuiz = QuizMarvel[0].quizz[quizz]
         if (fetchedArrayQuiz.length >= this.state.maxQuestions) {
+
+            this.storedDataRef.current = fetchedArrayQuiz
 
            const newArray = fetchedArrayQuiz.map(({answer, ...keepRest}) => keepRest)
            
@@ -41,15 +48,51 @@ class Quiz extends Component {
                 options: this.state.storedQuestions[this.state.idQuestion].options,
             })
         }
+
+        if (this.state.idQuestion !== prevState.idQuestion) {
+            this.setState({
+                question: this.state.storedQuestions[this.state.idQuestion].question,
+                options: this.state.storedQuestions[this.state.idQuestion].options,
+                userAnswer: null,
+                btnDisabled: true
+            })
+        }
+    }
+
+    nextQuestion = () => {
+        if (this.state.idQuestion === this.state.maxQuestions - 1) {
+
+        } else {
+            this.setState(prevState => ({
+                idQuestion: prevState.idQuestion + 1
+            }))
+        }
+
+        const goodAnswer = this.storedDataRef.current[this.state.idQuestion].answer
+        if (this.state.userAnswer === goodAnswer) {
+            this.setState(prevState => ({
+                score: prevState.score + 1
+            }))
+        }
+
+    }
+
+    submitAnswer = selectedAnswer => {
+        this.setState({
+            userAnswer: selectedAnswer,
+            btnDisabled: false
+        })
     }
 
     render() {
 
-        const { pseudo } = this.props.pseudo
-
         const displayOptions = this.state.options.map((option, index) => {
             return (
-                <p key={index} className='answerOptions'>{option}</p>
+                <p key={index} 
+                   className={`answerOptions ${this.state.userAnswer === option ? "selected" : null}`}
+                   onClick={() => this.submitAnswer(option)}
+                   >
+                    {option}</p>
             )
         })
 
@@ -59,7 +102,7 @@ class Quiz extends Component {
                 <ProgessBar />
                 <h2>{this.state.question}</h2>
                 {displayOptions}
-                <button className="btnSubmit">Suivant</button>
+                <button disabled={this.state.btnDisabled} onClick={this.nextQuestion} className="btnSubmit">Suivant</button>
             </div>
           )
     }
