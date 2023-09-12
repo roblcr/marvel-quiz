62749f9792e264926157815c9aecde0e7d6a71af
Robin Lecullier <lecullier.robin@gmail.com>
2023-09-12 19:32:59 +0200

    table results

--- src/components/QuizOver/index.js
+++ src/components/QuizOver/index.js
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const QuizOver = () => {
+const QuizOver = React.forwardRef((props, ref) => {
+
+    const [asked, setAsked] = useState([])
+
+    useEffect(() => {
+      setAsked(ref.current)
+    }, [ref])
+
+    const questionAnswer = asked.map( question => {
+        return (
+            <tr key={question.id}>
+                <td>{question.question}</td>
+                <td>{question.answer}</td>
+                <td><button className='btnInfo'>Infos</button></td>
+            </tr>
+        )
+    })
+    
+
   return (
-    <div>
-      QuizOver
+    <>
+    <div className='stepsBtnContainer'>
+        <p className="successMsg">Bravo ! Vous êtes un expert !</p>
+        <button className="btnResult success">Niveau suivant</button>
     </div>
-  )
-}
+    <div className="percentage">
+        <div className="progressPercent">Réussite: 10%</div>
+        <div className="progressPercent">Note: 10/10</div>
+    </div>
 
-export default QuizOver
+    <hr />
+    <p>Les réponses aux questions posées:</p>
+
+    <div className="answerContainer">
+        <table className="answers">
+            <thead>
+                <tr>
+                    <th>Questions</th>
+                    <th>Réponses</th>
+                    <th>Infos</th>
+                </tr>
+            </thead>
+            <tbody>
+                {questionAnswer}
+            </tbody>
+        </table>
+    </div>
+    </>
+  )
+})
+
+export default React.memo(QuizOver)
