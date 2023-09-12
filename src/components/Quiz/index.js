2ee7d1cc7c8d0c4c6a4b6db7c89cc74fceb1eab2
Robin Lecullier <lecullier.robin@gmail.com>
2023-09-12 15:54:22 +0200

    display question & option

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,21 +1,64 @@
 import React, { Component } from 'react'
 import Levels from '../Levels'
 import ProgessBar from '../ProgressBar'
+import { QuizMarvel } from '../quizMarvel'
 
 class Quiz extends Component {
+
+    state = {
+        levelNames: ['debutant', 'confirme', 'expert'],
+        quizLevel: 0,
+        maxQuestions: 10,
+        storedQuestions: [],
+        question: null,
+        options: [],
+        idQuestion: 0
+    }
+
+    loadQuestions = quizz => {
+        const fetchedArrayQuiz = QuizMarvel[0].quizz[quizz]
+        if (fetchedArrayQuiz.length >= this.state.maxQuestions) {
+
+           const newArray = fetchedArrayQuiz.map(({answer, ...keepRest}) => keepRest)
+           
+           this.setState({
+            storedQuestions: newArray
+           })
+
+        } else {
+            console.log('pas assez de questions')
+        }
+    }
+
+    componentDidMount() {
+        this.loadQuestions(this.state.levelNames[this.state.quizLevel])
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.storedQuestions !== prevState.storedQuestions) {
+            this.setState({
+                question: this.state.storedQuestions[this.state.idQuestion].question,
+                options: this.state.storedQuestions[this.state.idQuestion].options,
+            })
+        }
+    }
+
     render() {
 
         const { pseudo } = this.props.pseudo
+
+        const displayOptions = this.state.options.map((option, index) => {
+            return (
+                <p key={index} className='answerOptions'>{option}</p>
+            )
+        })
 
         return (
             <div>
                 <Levels />
                 <ProgessBar />
-                <h2>Notre Question Quiz</h2>
-                <p className='answerOptions'>Reponse 1</p>
-                <p className='answerOptions'>Reponse 2</p>
-                <p className='answerOptions'>Reponse 3</p>
-                <p className='answerOptions'>Reponse 4</p>
+                <h2>{this.state.question}</h2>
+                {displayOptions}
                 <button className="btnSubmit">Suivant</button>
             </div>
           )
